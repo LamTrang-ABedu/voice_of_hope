38550f605f3b608f0c4ea9c82e107b73c47dc669
lampv <lampv01203@gmail.com>
2025-05-08 03:34:14 +0700

    a

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ import uuid
 import tempfile
 from io import BytesIO
 from dotenv import load_dotenv
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)  # Cho phép tất cả origin gọi API này
 load_dotenv()
 
 app = Flask(__name__)
